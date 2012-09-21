dba69176ffe5291e82840b149d940fd21f7b78d4
roger.castaldo@gmail.com <roger.castaldo@gmail.com@5ba71561-c09d-595e-3941-65399a46788c>
2012-09-21 13:11:14 +0000

    added in ability to specify an image for the edit and delete buttons in a view.

---
+++ trunk/Attributes/DeleteButtonImagePath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.BackBoneDotNet.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DeleteButtonImagePath : Attribute
+    {
+        private string _url;
+        public string URL
+        {
+            get { return _url; }
+        }
+
+        private string _host;
+        public string Host
+        {
+            get { return _host; }
+        }
+
+        public DeleteButtonImagePath(string url)
+            : this(null, url) { }
+
+        public DeleteButtonImagePath(string host,string url)
+        {
+            _host = (host == null ? "*" : (host == "" ? "*" : host));
+            _url = url;
+        }
+    }
+}
