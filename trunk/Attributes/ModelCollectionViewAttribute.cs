a816a1bee09567a196d879f370215b376ca6c757
roger.castaldo@gmail.com <roger.castaldo@gmail.com@5ba71561-c09d-595e-3941-65399a46788c>
2013-03-05 17:25:00 +0000

    added in new attribute to allow specification of attributes to a models collection view

---
+++ trunk/Attributes/ModelCollectionViewAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.BackBoneDotNet.Attributes
+{
+    /*
+     * Used to specify special attributes to attach to a model collection view
+     */
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ModelCollectionViewAttribute : Attribute
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        private string _value;
+        public string Value
+        {
+            get { return _value; }
+        }
+
+        public ModelCollectionViewAttribute(string name, string value)
+        {
+            _name = name;
+            _value = value;
+        }
+    }
+}
