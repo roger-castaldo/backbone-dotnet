cbd65b1bbfec78ec92f79ad5176412a16133fc6d
roger.castaldo <roger.castaldo@5ba71561-c09d-595e-3941-65399a46788c>
2014-03-12 20:31:42 +0000

    added code to allow for null responses from exposed method calls to avoid them being an error when the response is legitamitely null

--- trunk/Attributes/ExposedMethod.cs
+++ trunk/Attributes/ExposedMethod.cs
@@ -7,5 +7,19 @@ namespace Org.Reddragonit.BackBoneDotNet.Attributes
     [AttributeUsage(AttributeTargets.Method)]
     public class ExposedMethod : Attribute
     {
+        private bool _allowNullResponse;
+        public bool AllowNullResponse
+        {
+            get { return _allowNullResponse; }
+        }
+
+
+        public ExposedMethod() :
+            this(false)
+        { }
+
+        public ExposedMethod(bool allowNullResponse){
+            _allowNullResponse=allowNullResponse;
+        }
     }
 }
