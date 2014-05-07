4699de7355ac858665e09db9b384595c93475497
roger.castaldo <roger.castaldo@5ba71561-c09d-595e-3941-65399a46788c>
2014-05-07 12:52:52 +0000

    added some additional commenting for the IHttpRequest implementations

--- Interfaces/IHttpRequest.cs
+++ Interfaces/IHttpRequest.cs
@@ -12,15 +12,27 @@ namespace Org.Reddragonit.BackBoneDotNet.Interfaces
      */
     public interface IHttpRequest
     {
+        //The url of the request
         Uri URL { get; }
+        //the method call 
         string Method { get; }
+        //The content (either Query String, or Post Content) passed to the request
         string ParameterContent { get; }
+        //Called to set the response content type
         void SetResponseContentType(string type);
+        //Called to write content to the underlying http request
         void WriteContent(string content);
+        //Called to Send the completed request response
         void SendResponse();
+        //Called to set the Response Status Number
         void SetResponseStatus(int statusNumber);
+        //Returns the Accept Language Header value for error language detection
         string AcceptLanguageHeaderValue { get; }
+        //Returns any additional Backbone variables for the request, if needed
         Hashtable AdditionalBackboneVariables { get; }
+        /*The functions below are used to test for security calls for each of the function type, a model 
+         * is supplied for non-static calls as well as the submitted parameters and ids for static ones
+        */
         bool IsLoadAllowed(Type model,string id, out int HttpStatusCode, out string HttpStatusMessage);
         bool IsLoadAllAllowed(Type model, out int HttpStatusCode, out string HttpStatusMessage);
         bool IsListAllowed(Type model, out int HttpStatusCode, out string HttpStatusMessage);
