2e16aae598aa29461c3470742cce3c86d629fc58
roger.castaldo <roger.castaldo@5ba71561-c09d-595e-3941-65399a46788c>
2014-10-07 20:13:12 +0000

    corrected bug with loading types in utility

--- Utility.cs
+++ Utility.cs
@@ -134,7 +134,8 @@ namespace Org.Reddragonit.BackBoneDotNet
                     }
                     catch (Exception e)
                     {
-                        if (e.Message != "The invoked member is not supported in a dynamic assembly.")
+                        if (e.Message != "The invoked member is not supported in a dynamic assembly."
+                            &&e.Message!="Unable to load one or more of the requested types. Retrieve the LoaderExceptions property for more information.")
                         {
                             throw e;
                         }
